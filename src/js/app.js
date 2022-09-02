521273a914e3b9ffc92d6816f45c6472b1a3f500
ikip1924 <rifkyrahman0420@gmail.com>
2022-09-02 08:50:04 +0700

    my changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,9 @@
 import "../css/app.css";
+import "../js/menu-toggler";
+import "../js/modal";
+import "../js/anchor-link-smooth";
+import "../js/carousel";
+import "../js/accordion";
+import "../js/slider";
+import "../js/shoppingCart";
+import "../js/shippingDetail";
